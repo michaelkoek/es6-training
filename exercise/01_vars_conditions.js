4654558ef52c4d1f6d438732ad773384d7c45275
Michael Koek <michaelkoek@gmail.com>
2018-02-09 12:18:13 +0100

    update the exercise description of task 1

--- exercise/01_vars_conditions.js
+++ exercise/01_vars_conditions.js
@@ -1,23 +1,24 @@
 /**
  * VARIABLES, CONDITIONS - TEST
- * theme: Famous Composer
+ * Theme: Famous Composer
  *
- * 1: Convert these words into variables using let/const:
+ * 1: Convert this data into variables using let/const:
  * - First name : Wolfgang
  * - Last name : Mozart
  * - Place of Birth : Salzburg
  * - Age : 57
  * - Year of Death : 1791
  *
- * 2: Re-assign one the variables to a new value
+ * 2: Re-assign one of the variables to a new value
  *
  * 3: Store the variables into an object
  *    - Use the shorthand
  *
- * 4: Create the following sentence and log this
- * - Create a function and pass the object as an argument
- * - output this sentence:
- * `[fn] [ln] was born in [pob] and died in [yod] at the age of [age]`
+ * 4: Create the following sentence and log it to the screen:
+ *    - Create a function and pass the object as an argument
+ *    - output this sentence:
+ *      "[fn] [ln] was born in [pob] and died in [yod] at the age of [age]"
+ *
  *
  * 5: Use the first name variable to check if the name is Wolfgang
  *    - TRUE -> set to -> Beethoven
@@ -25,5 +26,5 @@
  *
  * 6: Create a new condition to check if value of the previous task
  * resulted into 'Beethoven'.
- *    - TRUE -> log sentence: `Beethoven was born in Bonn`
+ *    - TRUE -> log sentence: "Beethoven was born in Bonn"
  */
