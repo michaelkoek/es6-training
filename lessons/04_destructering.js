02644bbbd8d088b9cc5a9d5da3a3887dbe0f6ef0
Michael Koek <michaelkoek@gmail.com>
2018-02-11 00:04:37 +0100

    desctructering lessons update

--- lessons/04_destructering.js
+++ lessons/04_destructering.js
@@ -13,20 +13,33 @@ const agent = {
 
 const firstName = agent.firstName;
 console.log(agent.lastName);
+/**
+ * You can change the const name of an object value
+ * You can give default values
+ */
 const { firstName: name, lastName, age, sex, codeName = '007' } = agent;
 
 console.log(name, lastName, age, sex, codeName);
 
+// Destructure array
 const arr = ['Goldmember', 'baron'];
 const [villanName, villanSurname] = arr;
 
 console.log(villanName, villanSurname);
 
+/**
+ *
+ * Force the order of the output
+ */
 const fullName = ({firstname, lastname}) => {
   console.log(firstname, lastname);
 };
 
+// Put the value in an object to force the output
 fullName({lastname: 'Bond', firstname: 'James'});
 
-const { firstName: fname, lastName: lname, ...ruben } = agent;
-console.log(fname, lname, ruben);
+/**
+ * Spread the rest of the values inside an object
+ */
+const { firstName: fname, lastName: lname, ...restAgentData } = agent;
+console.log(fname, lname, restAgentData);
